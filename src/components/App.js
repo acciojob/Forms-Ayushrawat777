84dfe2507c90b32b50343e7a7eac49fa1cf23199
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-23 12:07:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -71,37 +71,7 @@ const FormRef = () => {
   );
 };
 
-// FormState.js
-const FormState = () => {
-  const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Submitted", formData);
-  };
-
-  return (
-    <div className="card">
-      <h2>Form with useState</h2>
-      <form id="info-form" onSubmit={handleSubmit}>
-        <input name="fullName" id="full_name" type="text" placeholder="Full Name" onChange={handleChange} />
-        <input name="email" id="email" type="email" placeholder="Email" onChange={handleChange} />
-        <input name="password" id="password" type="password" placeholder="Password" onChange={handleChange} />
-        <input name="confirmPassword" id="password_confirmation" type="password" placeholder="Confirm Password" onChange={handleChange} />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
 
 
 

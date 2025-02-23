f725b2ca234c8c9b29b5ea8650164530d18a38f4
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-23 21:12:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,33 @@
-import React, { useRef, useState } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import "../styles/App.css";
-import Section3 from "./Section3";
-import Section1 from "./Section1";
-import Section2 from "./Section2";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Form from './Form';
+import FormRef from './FormRef';
+import FormState from './FormState';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="app-container">
-        <h1>Form Handling App</h1>
-        <nav>
-          <Link to="/form" id="form-link">Form</Link>
-          <Link to="/form-ref" id="form-ref-link">Form with useRef</Link>
-          <Link to="/form-state" id="form-state-link">Form with useState</Link>
-        </nav>
-        <Switch>
-          <Route path="/form" component={Section1} />
-          <Route path="/form-ref" component={Section2} />
-          <Route path="/form-state" component={Section3} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link id="form-link" to="/form">Form</Link>
+          </li>
+          <li>
+            <Link id="form-ref-link" to="/form-ref">Form Ref</Link>
+          </li>
+          <li>
+            <Link id="form-state-link" to="/form-state">Form State</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/form" element={<Form />} />
+        <Route path="/form-ref" element={<FormRef />} />
+        <Route path="/form-state" element={<FormState />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
